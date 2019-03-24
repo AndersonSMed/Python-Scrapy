7119ccea26d9b70749e513a16b56acac1fc3fbcc
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-03-24 20:39:29 -0300

    Added some more data to the crawler, also added xpath syntax in the selectors

--- src/app.py
+++ src/app.py
@@ -8,6 +8,12 @@ class BricketSetSpider (scrapy.Spider):
         SET_SELECTOR = '.set'
         for brickset in response.css(SET_SELECTOR):
             NAME_SELECTOR = 'h1 a ::text'
+            PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
+            IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
                 'name': brickset.css(NAME_SELECTOR).extract()[-1],
+                'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
+                'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
+                'image': brickset.css(IMAGE_SELECTOR).extract_first(),
             }
