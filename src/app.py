71835575e2b019d2ff247abec1d87291fd1fc7a6
Anderson Santos <anderson.santos.medeiros@hotmail.com>
2019-03-24 21:14:33 -0300

    Now getting more information

--- src/app.py
+++ src/app.py
@@ -2,18 +2,23 @@ import scrapy
 
 class BricketSetSpider (scrapy.Spider):
     name = 'brickset_spider'
-    start_urls = ['http://brickset.com/sets/year-2018']
+    first_year = 2016
+    start_urls = ['http://brickset.com/sets/year-' + str(first_year)]
 
     def parse(self, response):
         SET_SELECTOR = '.set'
         for brickset in response.css(SET_SELECTOR):
             NAME_SELECTOR = 'h1 a ::text'
             PIECES_SELECTOR = './/dl[dt/text() = "Pieces"]/dd/a/text()'
+            RRP_SELECTOR = './/dl[dt/text() = "RRP"]/dd/text()'
+            TAGS_SELECTOR = 'div span a ::text'
             MINIFIGS_SELECTOR = './/dl[dt/text() = "Minifigs"]/dd[2]/a/text()'
             IMAGE_SELECTOR = 'img ::attr(src)'
             yield {
                 'name': brickset.css(NAME_SELECTOR).extract()[-1],
                 'pieces': brickset.xpath(PIECES_SELECTOR).extract_first(),
+                'rrp': brickset.xpath(RRP_SELECTOR).extract_first(),
+                'tags': brickset.css(TAGS_SELECTOR).extract(),
                 'minifigs': brickset.xpath(MINIFIGS_SELECTOR).extract_first(),
                 'image': brickset.css(IMAGE_SELECTOR).extract_first(),
             }
@@ -25,3 +30,14 @@ class BricketSetSpider (scrapy.Spider):
                 response.urljoin(next_page),
                 callback=self.parse
             )
+
+        else:
+            self.first_year += 1
+            next_page = 'http://brickset.com/sets/year-' + str(self.first_year)
+            try:
+                yield scrapy.Request(
+                    response.urljoin(next_page),
+                    callback=self.parse
+                )
+            except:
+                pass
